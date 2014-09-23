dde5218c96e11a7f560adf46b2304e4f63b31335
miglard <dominic.miglar@edu.htl-villach.at>
2014-09-23 07:15:58 +0200

    angularjs

---
+++ avocado_frontend/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var avocadoApp = angular.module('avocadoApp', [
+    'ngRoute',
+    'ngResource'
+    ])
+
+avocadoApp.config(['$routeProvider', '$locationProvider', '$httpProvider',
+    function AppConfig($routeProvider, $locationProvider, $httpProvider) {
+        $routeProvider.
+        when('/', {
+            title: 'avocado Home',
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'    
+        }).
+        when('/customers/', {
+            title: 'Kundenverwaltung',
+            templateUrl: 'views/customers.html',
+            controller: 'CustomersCtrl'
+        }).
+        when('/customers/:customer_id', {
+            title: 'Kunde - Detail'
+            templateUrl: 'views/customer.html',
+            controller: 'CustomerCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+
+        $httpProvider.defaults.xsrfCookieName = 'djangotoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    }]);
+
+avocadoApp.run(['$rootScope', '$http', '$cookieStore',
+    function($rootScope, $http, $cookieStore) {
+        if($cookieStore.get('djangotoken')) {
+            $http.defaults.headers.commo.Authorization = 'Token ' + $cookieStore.get('djangotoken');
+            $http.defaults.headers.post.Authorization = 'Token ' + $cookieStore.get('djangotoken');
+        }
+    }]);
---
+++ avocado_frontend/scripts/conf.js
@@ -0,0 +1,5 @@
+(function(exports) {
+	var apiServer = 'http://127.0.0.1:8000/';
+	exports.apiUrl = apiServer + 'api/v1/';
+	exports.apiAuthUrl = apiServer + 'api-token-auth/';
+})(typeof exports === 'undefined' = this['configuration'] = {} : exports)
