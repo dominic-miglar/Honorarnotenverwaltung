add25c5aa5a16021c859369d9b0e598443171035
Dominic Miglar <dominic.miglar@w1r3.net>
2014-11-04 15:25:49 +0100

    Created pre-version of the Create-New-Invoice view (modal), issuer is always user with userprofile with id 1

--- avocado_frontend/scripts/controllers/InvoicesCtrl.js
+++ avocado_frontend/scripts/controllers/InvoicesCtrl.js
@@ -15,7 +15,7 @@ consumedServicesCtrl.controller('InvoicesCtrl', ['$rootScope', '$scope', '$locat
           });
       };*/
 
-      $scope.createNewConsumedService = function(newConsumedService) {
+      /*$scope.createNewInvoice = function(newConsumedService) {
         newConsumedService.customer = newConsumedService.customer.id;
         newConsumedService.service = newConsumedService.service.id;
         if (newConsumedService.invoice != undefined)
@@ -30,7 +30,7 @@ consumedServicesCtrl.controller('InvoicesCtrl', ['$rootScope', '$scope', '$locat
           console.log('Failed to create new consumed service!');
           $scope.updateView();
         });
-    };
+    };*/
 
       $scope.updateView = function() {
         Api.getCustomers().then(
@@ -51,33 +51,34 @@ consumedServicesCtrl.controller('InvoicesCtrl', ['$rootScope', '$scope', '$locat
       };
 
 
-      $scope.verifyNewConsumedServiceInput = function() {
-      // initial value
-      inputValid = false;
-      valid = [];
+      $scope.verifyNewInvoiceInput = function() {
+        inputValid = false;
+        valid = [];
 
-      valid.service = $scope.formNewConsumedService.service.$valid;
-      valid.customer = $scope.formNewConsumedService.customer.$valid;
-
-      if ($scope.newConsumedService != undefined) {
-        if($scope.newConsumedService.service.billing_type == "HR")
-          valid.consumed = $scope.formNewConsumedService.consumed.$valid;
-        else {
-          valid.consumed = true;
+        valid.customer = $scope.formNewInvoice.customer.$valid;
+        
+        if(valid.customer) {
+          inputValid = true;
         }
-      }
-      
-      valid.date = $scope.formNewConsumedService.date.$valid;
-      valid.time = $scope.formNewConsumedService.time.$valid;
 
-      if(valid.service && valid.customer && valid.consumed && valid.date && valid.time) {
-        inputValid = true;
-      }
+        return inputValid;
+      };
 
-      return inputValid;
-    };
+      $scope.createNewInvoice = function(newInvoice) {
+        newInvoice.customer = newInvoice.customer.id;
+        // TODO:
+        newInvoice.issuer = 1;
+        promise = Api.createNewInvoice(newInvoice);
+        promise.then(function(result) {
+          console.log('Successfully created new invoice.');
+          $scope.updateView();
+        },
+        function(result) {
+          console.log('Failed to create new invoice.')
+          $scope.updateView();
+        });
+      };
 
-      //$scope.updateServices();
       $scope.updateView();
-  }
+  } 
 ]);
